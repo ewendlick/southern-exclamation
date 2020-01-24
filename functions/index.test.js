d39acfa248d0352c211821d2e4c360c01704b632
Traviss Leavitt <traviss@comvex.co.jp>
2020-01-24 15:49:42 +0900

    Updated tests to cover more cases

--- functions/index.test.js
+++ functions/index.test.js
@@ -1,22 +1,53 @@
-const southernShock = require("./index");
+const consts = require("./constants");
+const { getPossibleTargets } = require("./index");
+
+jest.mock('./constants', () => ({
+  ALLOW_FALLBACKS: true,
+  RATINGS: {
+    test: ['rating1', 'rating2'],
+    test1: []
+  }
+}))
 
 const mockSentences = {
-    general: ['test']
+  rating1: ['rating1_sentence'],
+  rating2: ['rating2_sentence'],
+  general: ['general_sentence']
 }
 
 describe("getPossibleTargets()", () => {
-  it("throws error when target rating not found", () => {
-    try {
-        southernShock.getPossibleTargets('test', 'test')
-    } catch(error) {
-        expect(error.message).toBe('Invalid targetRating test!')
-    }
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it("throws exception when target rating not found", () => {
+    expect(() => {
+      getPossibleTargets('test', 'invalid')
+    }).toThrowError('Invalid targetRating invalid!')
   });
 
   it("returns correct targets", () => {
-    const possibleTargets = southernShock.getPossibleTargets(mockSentences, 'general')
+    const targets = getPossibleTargets(mockSentences, 'test')
+    expect(targets).toEqual(["rating1_sentence", "rating2_sentence"])
+  })
 
-    expect(possibleTargets.length).toBe(1)
-    expect(possibleTargets).toEqual(['test'])
+  it("falls back to general value", () => {
+    const targets = getPossibleTargets(mockSentences, 'test1')
+    expect(targets).toEqual(["general_sentence"])
+  })
+
+  it('does not allow fallback value when "ALLOW_FALLBACKS" is false', () => {
+    // This test needs to re-import the function so when it runs
+    // "ALLOW_FALLBACKS" is false
+    jest.mock('./constants', () => ({
+      ALLOW_FALLBACKS: false,
+      RATINGS: { test1: [] }
+    }))
+
+    const { getPossibleTargets } = require("./index");
+  
+    expect(() => {
+      getPossibleTargets(mockSentences, 'test1')
+    }).toThrowError("No values found!")
   })
 });
