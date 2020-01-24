25b3e33165fc2787530aa9f5237964714c1810bf
Traviss Leavitt <traviss@comvex.co.jp>
2020-01-24 13:13:38 +0900

    Added some tests for getPossibleTargets

--- functions/index.test.js
+++ functions/index.test.js
@@ -1,11 +1,22 @@
-const { getPossibleTargets } = require("./index");
+const southernShock = require("./index");
+
+const mockSentences = {
+    general: ['test']
+}
 
 describe("getPossibleTargets()", () => {
   it("throws error when target rating not found", () => {
     try {
-        getPossibleTargets('test', 'test')
+        southernShock.getPossibleTargets('test', 'test')
     } catch(error) {
         expect(error.message).toBe('Invalid targetRating test!')
     }
   });
+
+  it("returns correct targets", () => {
+    const possibleTargets = southernShock.getPossibleTargets(mockSentences, 'general')
+
+    expect(possibleTargets.length).toBe(1)
+    expect(possibleTargets).toEqual(['test'])
+  })
 });
