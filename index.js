9cb53448c6029d5b865144ad00161365978a068e
Traviss Leavitt <travissleavitt@gmail.com>
2020-12-17 20:17:29 +0900

    feat: Heroku
    - Removed Firebase
    - Added Prettier

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const functions = require('firebase-functions')
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const {ALLOW_FALLBACKS, ALLOW_RATING_OVERRIDE, RATINGS, VERB_OBJECT_COMBOS, NAMES} = require('./constants')
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const {
+  ALLOW_FALLBACKS,
+  ALLOW_RATING_OVERRIDE,
+  RATINGS,
+  VERB_OBJECT_COMBOS,
+  NAMES,
+  VERBS,
+  OBJECTS,
+} = require("./constants")
 
 const app = express()
 // Support URL-encoded bodies such as those in Slack
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }))
 
@@ -43,29 +53,35 @@ const getSentence = (rating) => {
 }
 
 const buildFromVerbObjectCombo = (rating) => {
-  return `Well, ${getRandomFrom(VERB_OBJECT_COMBOS, rating)} and call me ${getRandomFrom(NAMES, rating)}`
+  return `Well, ${getRandomFrom(VERB_OBJECT_COMBOS, rating)} and call me ${getRandomFrom(
+    NAMES,
+    rating
+  )}`
 }
 
 const buildFromVerbAndObject = (rating) => {
-  return `Well, ${getRandomFrom(VERBS, rating)} ${getRandomFrom(OBJECTS, rating)} and call me ${getRandomFrom(NAMES, rating)}`
+  return `Well, ${getRandomFrom(VERBS, rating)} ${getRandomFrom(
+    OBJECTS,
+    rating
+  )} and call me ${getRandomFrom(NAMES, rating)}`
 }
 
 const probabilities = [
   {
     weight: 10,
-    action: getSentence
+    action: getSentence,
   },
   {
     weight: 2,
-    action: buildFromVerbObjectCombo
+    action: buildFromVerbObjectCombo,
   },
   {
     weight: 50,
-    action: buildFromVerbAndObject
-  }
+    action: buildFromVerbAndObject,
+  },
 ]
 
-const run = (req, rating = 'general') => {
+const run = (req, rating = "general") => {
   // If "text" parameter exists and is valid, override rating
   if (ALLOW_RATING_OVERRIDE && req.route.methods.post && req.body.text) {
     const passedText = req.body.text.toLowerCase()
@@ -76,36 +92,32 @@ const run = (req, rating = 'general') => {
   }
 
   if (probabilities === undefined || probabilities[0] === undefined) {
-    throw new Error('Could not find probabilities!')
+    throw new Error("Could not find probabilities!")
   }
 
   const totalWeight = probabilities.reduce((acc, probability) => acc + probability.weight, 0)
   const targetWeight = Math.floor(Math.random() * totalWeight) // from 0 to (probabilties.length - 1)
 
-  for(let i = 0, currentWeight = 0; i < totalWeight; i++, currentWeight += probabilities[i].weight) {
+  for (
+    let i = 0, currentWeight = 0;
+    i < totalWeight;
+    i++, currentWeight += probabilities[i].weight
+  ) {
     if (probabilities[i + 1] === undefined || targetWeight < probabilities[i].weight) {
       return {
-        'response_type': 'in_channel',
-        'text': probabilities[i].action(rating)
+        response_type: "in_channel",
+        text: probabilities[i].action(rating),
       }
     }
   }
 
-  throw new Error('Unknown error')
+  throw new Error("Unknown error")
 }
 
+app.post("/", (req, res) => res.send(run(req, "general")))
+app.post("/all", (req, res) => res.send(run(req, "all")))
+app.post("/adult", (req, res) => res.send(run(req, "adult")))
 
-// NOTE: Slack only uses POST. GET is for testing functionality more easily in the browser
-app.get('/', (req, res) => res.send(run(req, 'general')))
-app.post('/', (req, res) => res.send(run(req, 'general')))
-
-app.get('/all', (req, res) => res.send(run(req, 'all')))
-app.post('/all', (req, res) => res.send(run(req, 'all')))
-
-app.get('/adult', (req, res) => res.send(run(req, 'adult')))
-app.post('/adult', (req, res) => res.send(run(req, 'adult')))
-
-exports.getPossibleTargets = getPossibleTargets
-
-// Expose Express API as a single Cloud Function
-exports.southernShockApi = functions.https.onRequest(app)
+app.listen(3000, () => {
+  console.log("Listening on port 3000")
+})
